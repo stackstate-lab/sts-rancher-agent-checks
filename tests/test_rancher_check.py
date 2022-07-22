16d0bfa317296493cd21e62102cdb6a3fb966d39
Ravan Naidoo <rnaidoo@stackstate.com>
2022-07-22 15:12:53 +0200

    Added support for rancher containers

--- tests/test_rancher_check.py
+++ tests/test_rancher_check.py
@@ -36,9 +36,9 @@ def test_check(m: requests_mock.Mocker = None):
     snapshot = topology.get_snapshot("")
     components = snapshot["components"]
     relations = snapshot["relations"]
-    assert len(components) == 40, "Number of Components does not match"
-    assert len(relations) == 33, "Number of Relations does not match"
-    assert len(health_check_states) == 33, "Number of Health does not match"
+    assert len(components) == 78, "Number of Components does not match"
+    assert len(relations) == 67, "Number of Relations does not match"
+    assert len(health_check_states) == 71, "Number of Health does not match"
     assert len(metric_names) == 0, "Number of Metrics does not match"
 
     # host_uid = "urn:host:/karbon-stackstate-c9a026-k8s-master-0"
